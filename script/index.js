fba480f08c6e603f4f8ba3700054c2158eeb71fc
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-03-01 10:35:00 +0200

    clear history done

--- script/index.js
+++ script/index.js
@@ -1,17 +1,22 @@
 document.getElementById("theme-icon").addEventListener('click', function () {
-
-
     const red = parseInt(Math.random() * 256);
     const green = parseInt(Math.random() * 256);
     const blue = parseInt(Math.random() * 256);
 
     if (red <= 255 && green <= 255 && blue <= 255) {
-        document.body.style.backgroundColor = `rgba(${red}, ${green}, ${blue})`;
+        document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
     }
-    else {
-        console.log("matha nosto!");
-    }
-
-
-
 })
+
+document.getElementById("clear-history").addEventListener("click", function () {
+    const historyContainer = document.getElementById("activity-history");
+    historyContainer.innerHTML = '';
+})
+
+document.getElementById('date').innerText = new Date().toLocaleDateString('en-GB', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+});;
+
+
